044c7ab2bce5162fa5046d1762a4dc4ba7d4b9f1
Pradiewradchada Peachaw <pradiewradchada@gmail.com>
2017-03-30 11:38:53 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<?php
+ $appid = "web592group03.appspot.com";
+ $page = $_GET['p'];
+ if($page=='') $page='main';
+ $title = $page;
+ function panel_include($title,$file,$ptype='default'){
+echo "<div class='panel panel-$ptype'>";
+echo "<div class='panel-heading'>$title</div>";
+echo "<div class='panel-body'>";
+if(file_exists($file)){
+ include($file);
+}else{
+ echo "ไม่พบไฟล์ $file ";
+}
+echo "</div>";
+echo "</div>";
+ use google\appengine\api\cloud_storage\CloudStorageTools; 
+ 
+function userpic($uid){    
+	global $appid;      
+	$userpic="gs://$appid/{$uid}.jpg";    
+	if(!file_exists($userpic)){        
+		return "user.png";    
+	}    
+	return CloudStorageTools::getImageServingUrl($userpic,["size"=>200]);   
+}
+?>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title><?= $page ?> </title>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <link rel="stylesheet"
+href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body role="document" background="#000">
+<header class="page-header text-center">
+<?php
+ readfile("gs://$appid/header.html");
+?>
+</header>
+<div class="container">
+<div class="col-sm-3">
+<?php panel_include("User","register.php"); ?>
+<?php panel_include("Menu","menu.php"); ?>
+</div>
+<div class="col-sm-9">
+<?php panel_include($title,"work8_body.php" ,"primary"); ?>
+</div>
+</div>
+ <footer class="text-center page-footer">
+ <?php
+ use google\appengine\api\users\UserService;
+ if (UserService::isCurrentUserAdmin()){
+ readfile("gs://$appid/footer.html");
+ echo "<br><a href='?p=edit&file=header.html' class='btn btn-default'>แก้ไข header</a>";
+ echo "<a href='?p=edit&file=footer.html' class='btn btn-default'>แก้ไข footer</a>";}
+?><br>
+กลุ่ม : ชื่ออะไรดีนะ<br>
+ภาควิชาวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์ มหาวิทยาลัยขอนแก่น
+ </footer>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+
+ use google\appengine\api\users\User;
+ use google\appengine\api\users\UserService;
+  global $user,$userdata,$appid;        
+  
+  $user  = UserService::getCurrentUser();    
+  if($user){        
+	$uid = $user->getUserId();        
+	$userfile  = "gs://$appid/user_$uid.json";        
+	$userdata = array();        
+	if(file_exists($userfile)){            
+       
+	$filedata = file_get_contents($userfile);           
+	$userdata = json_decode($filedata,true);         
+	}else{        
+	$userdata['nick']=$user->getNickname();     
+	}             
+	$url = UserService::createLogoutUrl('/home.php');        
+
+	echo "<img src='".userpic($uid)."' width='200'><br>";        
+	echo $userdata['nick'];        
+	echo "<br><a href='home.php?p=useredit'>Edit User</a>";          
+	echo "<br><a href='$url'>Logout</a>";   
+ 
+    }else{      
+	$url = UserService::createLoginUrl('/home.php');      
+	echo "<a href='$url'>Login or Register</a>";    } 
+?>
