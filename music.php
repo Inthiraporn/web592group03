71851684d3b46522f96b9752b7eb6f320f9cf757
Inthiraporn <hikidszaza@gmail.com>
2017-04-20 00:57:44 +0700

    Add files via upload

---
+++ music.php
@@ -0,0 +1,154 @@
+<IMG SRC="1.png"><h1><b style="color:#000000"><FONT SIZE=40><b>MUSIC</FONT></b></h1>
+
+<ul><li class='home'><a href="#a1"> <b style="color:#FFFFFF"><FONT SIZE=3>เพลงสตริง</li>
+  <li><a href="#a2"> <b style="color:#FFFFFF"><FONT SIZE=3>เพลงลูกทุ่ง</li>
+  <li><a href="#a3"> <b style="color:#FFFFFF"><FONT SIZE=3>เพลงเพื่อชีวิต</li>
+  <li><a href="#a4"> <b style="color:#FFFFFF"><FONT SIZE=3>เพลงสากล</li>
+ <li class='contact'><a href="#a5"> <b style="color:#FFFFFF"><FONT SIZE=3>เพลงเพื่อชีวิต</li></br></br></br></br></br>
+  
+<center><a name="a1"></center>
+
+<iframe width="400" height="350"src="https://www.youtube.com/embed/totIVAi9BUc?ecver=2" frameborder="0" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/wy-B-s4SGZY?ecver=2" frameborder="0" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/DN_jocY5GaI" frameborder="0" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/rBXPvLzMqpU?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/kTHcaTCM4rs?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/rfIMBjXCPZs?list=RDtotIVAi9BUc?ecver=2" allowfullscreen ></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/Sl1qbeUvibE?list=RDtotIVAi9BUc?ecver=2"  allowfullscreen></iframe> 
+<iframe width="400" height="350"src="https://www.youtube.com/embed/2VEfHYXhO8M?list=RDtotIVAi9BUc?ecver=2"  allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/IOg45dmtqPc?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/YM_D0Qx5VOk?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/skhqR3f-wDs?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/EEocnHtllJ8?list=RDtotIVAi9BUc?ecver=2"  allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/-gg5ns2fd4U?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/Z-qjxO_ZJKM?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/fmAEiuuoc_0?list=RDtotIVAi9BUc?ecver=2"  allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/QyhrOruvT1c?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/5Bh49ch5mw8?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/EPZfaGPP3cA?list=RDtotIVAi9BUc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/7RbCL_Eoj98?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/us0589k1shM?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/Jt8SMztJrx8?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/eOLyuzdqLcQ?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/h2nIHSgQ2Bs?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350"src="https://www.youtube.com/embed/4ilVdRM9WFo?ecver=2" allowfullscreen></iframe></br></br></br></br>
+
+
+<center><a name="a2"></center>
+<center><table width=50% cellpadding="5" cellspacing="3" style="border:1px solid #dcdc77;"><tr>
+<td bgcolor="#dcdc77"style="border-style:solid;border-width:4px;border-color:#dcdc77 gray gray #dcdc77;"><FONT SIZE=5>
+<center><b><b style="color:#330066">เพลงลูกทุ่ง</center></b></td></tr></table> </center></br></br></br></br></br>
+
+ 
+<iframe width="400" height="350" src="https://www.youtube.com/embed/cOXXbMB5L4A?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/yiDgLAl8soc?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/AzAbmDw_1YU?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/RmALkR-NpKs?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/cQKKEB-yvNE?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/xir5VPhlJ_M?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/-rtoXOBf8dI?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/pnkIYgqwsIE?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/9BrJeJMVAjg?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/PHm8plV7L2o?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/4O5S_7VJAm4?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/wY2s4i5Aon4?ecver=2" allowfullscreen></iframe>
+<iframe width="400" height="350" src="https://www.youtube.com/embed/2trZ3D6JzcY?ecver=2" allowfullscreen></iframe>
+
+
+
+
+
+
+
+
+</ul></br></br></br>
+
+
+
+<style>
+
+
+
+* {
+font-family: "Gill Sans Ultra Bold";
+}
+
+a{
+	text-decoration:none;
+}
+body {
+  font-family: 'Lucida Grande', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  padding: 20px 50px 150px;
+  font-size: 13px;
+  text-align: center;
+  background: #BEBEBE;
+}
+
+ul {
+  text-align: left;
+  display: inline;
+  margin: 0;
+  padding: 50px 40px 170px 0;
+  list-style: none;
+}
+ul li {
+  font: bold 12px/18px sans-serif;
+  display: inline-block;
+  margin-right: -1px;
+  position: relative;
+  padding: 20px 30px;
+  background: #000099;
+  cursor: pointer;
+  -webkit-transition: all 0.2s;
+  -moz-transition: all 0.2s;
+  -ms-transition: all 0.2s;
+  -o-transition: all 0.2s;
+  transition: all 0.2s;
+  color: #fff;
+}
+ul li:hover {
+  background: #3366FF;
+  color: #fff;
+}
+ul li ul {
+  padding: 0;
+  position: absolute;
+  top: 60px;
+  left: 0;
+  width: 150px;
+  -webkit-box-shadow: none;
+  -moz-box-shadow: none;
+  box-shadow: none;
+  display: none;
+  opacity: 0;
+  visibility: hidden;
+  -webkit-transiton: opacity 0.2s;
+  -moz-transition: opacity 0.2s;
+  -ms-transition: opacity 0.2s;
+  -o-transition: opacity 0.2s;
+  -transition: opacity 0.2s;
+}
+ul li ul li { 
+  background: #2C3E50; 
+  display: block; 
+  color: #fff;
+}
+
+ul li ul li:hover { background: #1abc9c; }
+ul li:hover ul {
+  display: block;
+  opacity: 1;
+  visibility: visible;
+}
+.home {
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
+}
+.contact {
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+}
+.ill {}
+</style>
+
+
